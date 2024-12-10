c50a8905631fd9b76becbb484aeb6a82c395b3aa
Harry White <harrywhiteproductions@gmail.com>
2024-12-11 09:30:45 +1000

    Fix error handling in texture loading code.

--- zf3/src/zf3_assets.c
+++ zf3/src/zf3_assets.c
@@ -100,45 +100,32 @@ static GLuint create_shader_prog_from_srcs(const char* const vertShaderSrc, cons
 
 static bool load_textures(FILE* const fs) {
     assert(i_assets);
-    assert(i_assets->texCnt > 0);
 
-    // Generate textures and store their IDs.
-    glGenTextures(i_assets->texCnt, i_assets->texGLIDs);
+    if (i_assets->texCnt > 0) {
+        // Allocate a buffer for temporarily storing the pixel data of each texture.
+        const int pxDataBufSize = ZF3_TEX_CHANNEL_COUNT * ZF3_TEX_WIDTH_LIMIT * ZF3_TEX_HEIGHT_LIMIT;
+        unsigned char* const pxDataBuf = malloc(pxDataBufSize);
 
-    // Read the sizes and pixel data of textures and finish setting them up.
-    const int pxDataBufSize = ZF3_TEX_CHANNEL_COUNT * ZF3_TEX_WIDTH_LIMIT * ZF3_TEX_HEIGHT_LIMIT;
-    unsigned char* const pxDataBuf = malloc(pxDataBufSize); // A buffer for temporarily storing the pixel data of each texture.
-
-    if (!pxDataBuf) {
-        return false;
-    }
-
-    bool err = false;
-
-    for (int i = 0; i < i_assets->texCnt; ++i) {
-        if (fread(&i_assets->texSizes[i], sizeof(i_assets->texSizes[i]), 1, fs) < 1) {
-            err = true;
-            break;
+        if (!pxDataBuf) {
+            return false;
         }
 
-        if (fread(pxDataBuf, ZF3_TEX_CHANNEL_COUNT * i_assets->texSizes[i].x * i_assets->texSizes[i].y, 1, fs) < 1) {
-            err = true;
-            break;
+        // Generate textures and store their IDs.
+        glGenTextures(i_assets->texCnt, i_assets->texGLIDs);
+
+        // Read the sizes and pixel data of textures and finish setting them up.
+        for (int i = 0; i < i_assets->texCnt; ++i) {
+            fread(&i_assets->texSizes[i], sizeof(i_assets->texSizes[i]), 1, fs);
+
+            fread(pxDataBuf, ZF3_TEX_CHANNEL_COUNT * i_assets->texSizes[i].x * i_assets->texSizes[i].y, 1, fs);
+
+            glBindTexture(GL_TEXTURE_2D, i_assets->texGLIDs[i]);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, i_assets->texSizes[i].x, i_assets->texSizes[i].y, 0, GL_RGBA, GL_UNSIGNED_BYTE, pxDataBuf);
         }
 
-        glBindTexture(GL_TEXTURE_2D, i_assets->texGLIDs[i]);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, i_assets->texSizes[i].x, i_assets->texSizes[i].y, 0, GL_RGBA, GL_UNSIGNED_BYTE, pxDataBuf);
-    }
-
-    free(pxDataBuf);
-
-    if (err) {
-        glDeleteTextures(i_assets->texCnt, i_assets->texGLIDs);
-        //memset(i_assets->texGLIDs, 0, sizeof(i_assets->texGLIDs));
-        //memset(i_assets->texSizes, 0, sizeof(i_assets->texSizes));
-        return false;
+        free(pxDataBuf);
     }
 
     return true;
