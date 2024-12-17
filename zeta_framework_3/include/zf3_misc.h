36d03d9d08c675f19cbdaa4be051286cbe836127
Harry White <harrywhiteproductions@gmail.com>
2024-12-17 11:15:01 +1000

    Add ActivityBuf utility struct.

--- zeta_framework_3/include/zf3_misc.h
+++ zeta_framework_3/include/zf3_misc.h
@@ -11,22 +11,81 @@ namespace zf3 {
         Byte bytes[bits_to_bytes(BIT_CNT)];
     };
 
+    template<typename T, int CNT>
+    struct ActivityBuf {
+        T buf[CNT];
+        Bitset<CNT> activity;
+
+        T& operator[](const int index);
+        const T& operator[](const int index) const;
+
+        T& get_even_if_inactive(const int index) {
+            return buf[index];
+        }
+
+        const T& get_even_if_inactive(const int index) const {
+            return buf[index];
+        }
+    };
+
     int get_first_inactive_bit_index(const Byte* const bytes, const int bitCnt);
     bool are_all_bits_active(const Byte* const bytes, const int bitCnt);
 
-    inline bool is_bit_active(const Byte* const bytes, const int bitIndex) {
-        return bytes[bitIndex / 8] & (1 << (bitIndex % 8));
-    }
-
     inline void activate_bit(Byte* const bytes, const int bitIndex) {
         bytes[bitIndex / 8] |= 1 << (bitIndex % 8);
+    }
+
+    template<int BIT_CNT>
+    inline void activate_bit(Bitset<BIT_CNT>& activity, const int bitIndex) {
+        activate_bit(activity.bytes, bitIndex);
     }
 
     inline void deactivate_bit(Byte* const bytes, const int bitIndex) {
         bytes[bitIndex / 8] &= ~(1 << (bitIndex % 8));
     }
 
+    template<int BIT_CNT>
+    inline void deactivate_bit(Bitset<BIT_CNT>& activity, const int bitIndex) {
+        deactivate_bit(activity.bytes, bitIndex);
+    }
+
     inline void clear_bits(Byte* const bytes, const int bitCnt) {
         memset(bytes, 0, bits_to_bytes(bitCnt));
+    }
+
+    template<int BIT_CNT>
+    inline void clear_bits(Bitset<BIT_CNT>& activity) {
+        clear_bits(activity.bytes, BIT_CNT);
+    }
+
+    inline bool is_bit_active(const Byte* const bytes, const int bitIndex) {
+        return bytes[bitIndex / 8] & (1 << (bitIndex % 8));
+    }
+
+    template<int BIT_CNT>
+    inline bool is_bit_active(const Bitset<BIT_CNT>& activity, const int bitIndex) {
+        return is_bit_active(activity.bytes, bitIndex);
+    }
+
+    template<int BIT_CNT>
+    inline int get_first_inactive_bit_index(const Bitset<BIT_CNT>& activity) {
+        return get_first_inactive_bit_index(activity.bytes, BIT_CNT);
+    }
+
+    template<int BIT_CNT>
+    inline bool are_all_bits_active(const Bitset<BIT_CNT>& activity) {
+        return are_all_bits_active(activity.bytes, BIT_CNT);
+    }
+
+    template<typename T, int CNT>
+    inline T& ActivityBuf<T, CNT>::operator[](const int index) {
+        assert(is_bit_active(activity, index));
+        return buf[index];
+    }
+
+    template<typename T, int CNT>
+    inline const T& ActivityBuf<T, CNT>::operator[](const int index) const {
+        assert(is_bit_active(activity, index));
+        return buf[index];
     }
 }
