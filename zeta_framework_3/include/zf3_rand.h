c20598ac8a1154346800642a7098fa9c04755ba7
Harry White <harrywhiteproductions@gmail.com>
2024-12-21 18:54:18 +1000

    Add overloads for random generation functions.

--- zeta_framework_3/include/zf3_rand.h
+++ zeta_framework_3/include/zf3_rand.h
@@ -9,4 +9,12 @@ namespace zf3 {
     int gen_rand_int(const int min, const int max);
     float gen_rand_float(const float min, const float max);
     float gen_rand_perc();
+
+    inline int gen_rand_int(const int max) {
+        return gen_rand_int(0, max);
+    }
+
+    inline float gen_rand_float(const float max) {
+        return gen_rand_float(0.0f, max);
+    }
 }
